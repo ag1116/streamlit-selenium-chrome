aa6e02b0680377523937afb2ab0da02de8315972
ag1116 <arnav1116@gmail.com>
2024-05-15 18:00:11 -0700

    revert

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,31 +29,8 @@ with st.echo():
     options = Options()
     options.add_argument("--disable-gpu")
     options.add_argument("--headless")
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
 
     driver = get_driver()
-    driver.get("https://www.iso-ne.com/isoexpress/web/charts/guest-hub?p_p_id=threedayforecastlist_WAR_isoneportlet_INSTANCE_942ZZ7qiGL8q&p_p_lifecycle=0&p_p_state=pop_up&p_p_mode=view&p_p_col_id=column-2&p_p_col_pos=5&p_p_col_count=6")
+    driver.get("http://example.com")
 
-    # Wait for the table to load
-    driver.implicitly_wait(2)
-
-    # Find the table by its tag name
-    table = driver.find_element_by_tag_name('table')
-    
-    if table:
-        # Find all rows in the table
-        rows = table.find_elements_by_tag_name('tr')
-        table_data = []
-
-        for row in rows:
-            # Find all columns in each row
-            cols = row.find_elements_by_tag_name('td')
-            cols = [col.text.strip() for col in cols]
-            table_data.append(cols)
-        
-        # Display the table data in Streamlit
-        st.write("Table Data:")
-        st.table(table_data)
-    
-    driver.quit()
+    st.code(driver.page_source)
